24d6b6b72216da723b2e97b59302b8b2437ce961
dbobb220 <david.bobb.220@gmail.com>
2019-10-05 18:53:59 -0500

    app

---
+++ public/precache-manifest.8e38aadca9267e466662be45b43e0e8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb376b86e5acf89612208494c4c6f600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b9653802e1a42c4590e",
+    "url": "/static/css/2.00ba8a3c.chunk.css"
+  },
+  {
+    "revision": "8081f1ef1ce972388f60",
+    "url": "/static/css/main.0f4ec70c.chunk.css"
+  },
+  {
+    "revision": "8b9653802e1a42c4590e",
+    "url": "/static/js/2.8905af1e.chunk.js"
+  },
+  {
+    "revision": "8081f1ef1ce972388f60",
+    "url": "/static/js/main.7a632caa.chunk.js"
+  },
+  {
+    "revision": "b4825f18316d06f693b9",
+    "url": "/static/js/runtime~main.9edf2231.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "fa3da3dedf97e6698cf52954cffd35e3",
+    "url": "/static/media/kanban_home.fa3da3de.png"
+  }
+]);
